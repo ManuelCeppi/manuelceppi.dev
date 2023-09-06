080deed72f320400c093b0627f0d0f7d3547d15e
Manuel Ceppi <manuel.ceppi@outlook.it>
2023-09-06 22:29:11 +0200

    improvements + styling

--- src/data.js
+++ src/data.js
@@ -18,7 +18,11 @@ export const headerData = {
       text: 'Contact Me',
       href: '',
     },
-
+  ],
+  socialLinks: [
+    { ariaLabel: 'Linkedin', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/in/manuel-ceppi/' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/m_ceppi/' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/ManuelCeppi' },
   ],
   actions: [
     // { type: 'button', text: 'Download', href: 'https://github.com/onwidget/astrowind' } Action example
