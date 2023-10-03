d098b347de9ad555ab39ba589c4cb65b3874daae
Manuel Ceppi <manuel.ceppi@outlook.it>
2023-10-03 21:37:55 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,7 +25,7 @@ export default defineConfig({
   base: SITE.basePathname,
   trailingSlash: SITE.trailingSlash ? 'always' : 'never',
 
-  output: 'static',
+  output: 'server',
   adapter: vercel({
     webAnalytics: {
       enabled: true,
