ff9af49c47970766b24a84aba705a7d586e3db92
Manuel Ceppi <manuel.ceppi@outlook.it>
2023-10-03 21:34:16 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
