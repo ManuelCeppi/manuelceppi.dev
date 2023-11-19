9d9fd0c46313d73e062139e0dca3341ece297dd9
Manuel Ceppi <manuel.ceppi@outlook.it>
2023-11-19 19:26:37 +0100

    project cleaning + improvements + layout change

--- src/data.js
+++ src/data.js
@@ -7,14 +7,6 @@ export const headerData = {
       text: t('nav.aboutMe'),
       href: '/about',
     },
-    // {
-    //   text: 'Blog',
-    //   href: getBlogPermalink(),
-    // },
-    {
-      text: t('nav.hobbies'),
-      href: '/hobbies',
-    },
     {
       text: t('nav.contactMe'),
       href: '',
@@ -24,9 +16,6 @@ export const headerData = {
     { ariaLabel: 'Linkedin', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/in/manuel-ceppi/' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/m_ceppi/' },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/ManuelCeppi' },
-  ],
-  actions: [
-    // { type: 'button', text: 'Download', href: 'https://github.com/onwidget/astrowind' } Action example
   ],
 };
 
