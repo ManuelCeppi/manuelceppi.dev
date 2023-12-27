c2879ab9dd772a229fda5bb819319c8dbcebd2ce
Manuel Ceppi <manuel.ceppi@outlook.it>
2023-12-27 22:26:05 +0100

    tailwind text animations + style improvements

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,23 +4,19 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      colors: {
-        primary: 'var(--aw-color-primary)',
-        secondary: 'var(--aw-color-secondary)',
-        accent: 'var(--aw-color-accent)',
-      },
-      fontFamily: {
-        sans: ['var(--aw-font-sans)', ...defaultTheme.fontFamily.sans],
-        serif: ['var(--aw-font-serif)', ...defaultTheme.fontFamily.serif],
-        heading: ['var(--aw-font-heading)', ...defaultTheme.fontFamily.sans],
-      },
       animation: {
-        border: 'background ease infinite',
+        text: 'text 5s ease infinite',
       },
       keyframes: {
-        background: {
-          '0%, 100%': { backgroundPosition: '0% 50%' },
-          '50%': { backgroundPosition: '100% 50%' },
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
         },
       },
     },
